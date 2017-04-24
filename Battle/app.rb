1e04ba9f51bd27e70d99ba287d919f4ac1eb7a1a
Timothy Robinson <timmyrobby@gmail.com>
2017-04-24 22:24:56 +0100

    Included hotfix for shotgun sessions issues

--- Battle/app.rb
+++ Battle/app.rb
@@ -3,19 +3,30 @@ require 'sinatra'
 class Battle < Sinatra::Base
   enable :sessions
 
+  # This allows shotgun to persist in storing session variables
+  configure(:development) { set :session_secret, "something" }
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    session[:player1] = params[:player1]
-    session[:player2] = params[:player2]
-    redirect to('/play')
+    session[:p1] = params[:player1]
+    session[:p2] = params[:player2]
+    redirect '/play'
   end
 
   get '/play' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = session[:p1]
+    @player2 = session[:p2]
     erb(:play)
   end
+
+  #
+  # get '/play' do
+  #   p "IN PLAY", session[:player2]
+  #   @player1 = session[:player1]
+  #   @player2 = session[:player2]
+  #   erb(:play)
+  # end
 end
--- Battle/spec/features/feature_test_spec.rb
+++ Battle/spec/features/feature_test_spec.rb
@@ -13,6 +13,17 @@ feature 'Feature test' do
       fill_in 'player2', with: 'Villain'
     end
     click_button 'submit'
-    expect(page).to have_content 'Player 1: Hero, Player 2: Villain'
+    expect(page).to have_content 'Player 1: Hero'
+    expect(page).to have_content 'Player 2: Villain'
+  end
+
+  it 'displays player 2 hit points' do
+    visit('/')
+    within('form') do
+      fill_in 'player1', with: 'Hero'
+      fill_in 'player2', with: 'Villain'
+    end
+    click_button 'submit'
+    expect(page).to have_content 'Player 2: Villain, Hit Points: 100'
   end
 end
