4078190cc865b1d03394e4e25eb97cbd129028c9
Timothy Robinson <timmyrobby@gmail.com>
2017-04-24 15:43:05 +0100

    Adds if statement

--- app.rb
+++ app.rb
@@ -9,14 +9,13 @@ get '/secret' do
   'This is a secret page'
 end
 
-get '/test' do
-  'Test page'
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
 
-get '/random' do
-  'asd;maspdma'
-end
-
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
