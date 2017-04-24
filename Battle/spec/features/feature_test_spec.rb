1ca8569664f945a3ff5f3ee8ae8211d9ff42ad9c
Timothy Robinson <timmyrobby@gmail.com>
2017-04-24 22:32:01 +0100

    Refactoring using a helper

--- Battle/spec/features/feature_test_spec.rb
+++ Battle/spec/features/feature_test_spec.rb
@@ -1,4 +1,5 @@
 require 'capybara'
+require_relative './web_helpers.rb'
 
 feature 'Feature test' do
   it 'has content on the page' do
@@ -7,23 +8,13 @@ feature 'Feature test' do
   end
 
   it 'allows players to fill in and see their names' do
-    visit('/')
-    within('form') do
-      fill_in 'player1', with: 'Hero'
-      fill_in 'player2', with: 'Villain'
-    end
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content 'Player 1: Hero'
     expect(page).to have_content 'Player 2: Villain'
   end
 
   it 'displays player 2 hit points' do
-    visit('/')
-    within('form') do
-      fill_in 'player1', with: 'Hero'
-      fill_in 'player2', with: 'Villain'
-    end
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content 'Player 2: Villain, Hit Points: 100'
   end
 end
---
+++ Battle/spec/features/web_helpers.rb
@@ -0,0 +1,8 @@
+def sign_in_and_play
+  visit('/')
+  within('form') do
+    fill_in 'player1', with: 'Hero'
+    fill_in 'player2', with: 'Villain'
+  end
+  click_button 'submit'
+end
